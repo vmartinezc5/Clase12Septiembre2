016630f5e074c1a5e756bd5f87b6b9d580487064
vmartinezc5 <110207678+vmartinezc5@users.noreply.github.com>
2022-09-12 21:23:51 -0600

    Add files via upload

--- VistaEstudiante.php
+++ VistaEstudiante.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- JavaScript Bundle with Popper -->
+    <script src="https://kit.fontawesome.com/a87f2bfe07.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/a87f2bfe07.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"><script src="https://kit.fontawesome.com/a87f2bfe07.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     <title>Document</title>
@@ -59,9 +61,9 @@
                 <td>  <?php echo $Estudiantes['apellidos'] ?> </td>
                 <td>  <?php echo $Estudiantes['direccion'] ?> </td>
                 <td>  <?php echo $Estudiantes['telefono'] ?> </td>
-                <td><a href="VistaEditarEstudiante.php?idEst=<?php echo $Estudiantes['idEstudiante'];?>">EDITAR</a></td>
-                <td><a href="VistaEliminarEstudiante.php?idEst=<?php echo $Estudiantes['idEstudiante'];?>">ELIMINAR</a></td>
-        </tr>
+                <script src="https://kit.fontawesome.com/a87f2bfe07.js" crossorigin="anonymous"></script>
+                <script src="https://kit.fontawesome.com/a87f2bfe07.js" crossorigin="anonymous"></script>   
+             </tr>
           
        <?php } ?>
     
